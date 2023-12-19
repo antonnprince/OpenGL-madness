b728a8f4fd99c5c00d3dc283d42dc16a2ce2f100
antonnprince <antonprince95@gmail.com>
2023-12-19 23:14:30 +0530

    trying out smtg

--- circle.py
+++ circle.py
@@ -1,24 +1,48 @@
 import math
-import time
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 
 def init():
-    gluOrtho2D(-300, 300, -300, 300)
+    gluOrtho2D(-400, 400, -300, 300)
     glClearColor(0, 0, 0, 1)
 
 
 def draw():
     glClear(GL_COLOR_BUFFER_BIT)
     glLineWidth(2)
-    glColor3f(1, 0, 1)
+
+    # Draw the first circle
     glBegin(GL_TRIANGLE_FAN)
-    """glVertex2f(40, 0)"""
+    glColor3f(1, 0, 1)
     for i in range(0, 361):
-        glVertex2f(50 * math.cos(i), 50 * math.sin(i))
+        theta = math.radians(i)
+        x = -40 + 10 * math.cos(theta)
+        y = 0 + 10 * math.sin(theta)
+        glVertex2f(x, y)
     glEnd()
+
+    # Draw the second circle
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(1, 0, 1)
+    for i in range(0, 361):
+        theta = math.radians(i)
+        x = 40 + 10 * math.cos(theta)
+        y = 0 + 10 * math.sin(theta)
+        glVertex2f(x, y)
+    glEnd()
+
+    # Draw the third circle
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(1, 1, 0)
+    for i in range(0, 361):
+        theta = math.radians(i)
+        x = 0 + 50 * math.cos(theta)
+        y = 0 + 50 * math.sin(theta)
+        glVertex2f(x, y)
+    glEnd()
+
     glFlush()
 
 
@@ -27,7 +51,7 @@ def main():
     glutInitWindowSize(500, 500)
     glutInitWindowPosition(600, 0)
     glutInitDisplayMode(GLUT_RGBA)
-    glutCreateWindow("CIRCLE")
+    glutCreateWindow("CIRCLES")
     glutDisplayFunc(lambda: draw())
     init()
     glutMainLoop()
